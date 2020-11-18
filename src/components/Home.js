b6ff8d59865629063d00e254b77c81cc6ec103e2
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2020-11-17 18:03:01 -0800

    MVP Done

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Home() {
+  return (
+    <div>
+      <div className="showcase">
+        <div className="img">
+            <Link to="/pizza" className="pizza">Pizza?</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
